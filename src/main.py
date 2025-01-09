e7d1de176ec933a7f910c84d78de85f154cdf1b0
Ekter <ektermoi1@gmail.com>
2025-01-09 11:57:01 +0100

    feat: turn on led at startup

--- src/main.py
+++ src/main.py
@@ -1,9 +1,17 @@
 import ftprci as fci
 import time
 
+import pygpio
 
+print("initializing LSM9DS1")
 acc = fci.sensor.LSM9DS1()
+print("went ok")
 
+led1 = pygpio.LED(21)
+
+led1.on()
+time.sleep(1)
+led1.off()
 acc.check()
 
 acc.full_settings()
