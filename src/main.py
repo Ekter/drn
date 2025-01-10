a84c6391305eb3d3bd2fbc10ec0bb3d2fbb0f4dd
Ekter <ektermoi1@gmail.com>
2025-01-11 00:56:18 +0100

    feat: reg6 lsm9ds1

--- src/main.py
+++ src/main.py
@@ -8,14 +8,35 @@ os.system("i2cdetect -y 1")
 
 print("initializing LSM9DS1")
 pi = pigpio.pi()
-pi.set_mode(10, pigpio.OUTPUT)
-pi.write(10, 1)                 # CS_AG
-pi.set_mode(24, pigpio.OUTPUT)
-pi.write(24, 1)                 # CS_M
-pi.set_mode(11, pigpio.OUTPUT)
-pi.write(11, 0)                 # SDO_AG
-pi.set_mode(9, pigpio.OUTPUT)
-pi.write(9, 0)                  # SD0_M
+
+pi.set_mode(10, pigpio.OUTPUT)  # CS_AG
+pi.write(10, 1)
+pi.set_mode(24, pigpio.OUTPUT)  # CS_M
+pi.write(24, 1)
+pi.set_mode(11, pigpio.OUTPUT)  # SDO_AG
+pi.write(11, 0)
+pi.set_mode(9, pigpio.OUTPUT)   # SD0_M
+pi.write(9, 0)
+
+pi.set_mode(16, pigpio.OUTPUT) # leds
+pi.write(16, 0)
+pi.set_mode(20, pigpio.OUTPUT)
+pi.write(20, 0)
+pi.set_mode(21, pigpio.OUTPUT)
+pi.write(21, 0)
+
+pi.set_mode(12, pigpio.OUTPUT) # mot1
+pi.write(12, 0)
+pi.set_mode(13, pigpio.OUTPUT) # mot2
+pi.write(13, 0)
+pi.set_mode(18, pigpio.OUTPUT) # mot3
+pi.write(18, 0)
+pi.set_mode(19, pigpio.OUTPUT) # mot4
+pi.write(19, 0)
+
+
+# http://abyz.me.uk/rpi/pigpio/python.html#set_servo_pulsewidth
+# http://abyz.me.uk/rpi/pigpio/python.html#set_PWM_range
 
 os.system("i2cdetect -y 1")
 
@@ -32,16 +53,13 @@ while True:
     print(acc.read())
     pi.write(21, 1)
     pi.write(20, 0)
-    time.sleep(0.5)
-    pi.write(21, 0)
-    pi.write(20, 1)
-    pi.write(16,1)
-    time.sleep(0.5)
-    print(acc.read())
-    pi.write(21, 1)
-    pi.write(20, 0)
-    time.sleep(0.5)
+    pi.write(16,0)
+    time.sleep(1/3)
     pi.write(21, 0)
     pi.write(20, 1)
     pi.write(16,0)
+    time.sleep(1/3)
+    pi.write(21, 0)
+    pi.write(20, 0)
+    pi.write(16,1)
     time.sleep(0.5)
