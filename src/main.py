3af3ff3cb10bc55e2c78d51c631e69cad563a3f2
Ekter <ektermoi1@gmail.com>
2025-01-13 23:08:36 +0100

    feat: 2-1-1 init

--- src/main.py
+++ src/main.py
@@ -49,16 +49,17 @@ pi.set_servo_pulsewidth(13, 1500) # mean throttle
 time.sleep(5)
 
 pi.set_servo_pulsewidth(18, 1000) # min throttle
-pi.set_servo_pulsewidth(19, 1000) # min throttle
 time.sleep(1)
 pi.set_servo_pulsewidth(18, 2000) # max throttle
-pi.set_servo_pulsewidth(19, 2000) # max throttle
-
 time.sleep(1)
-
 pi.set_servo_pulsewidth(18, 1500) # mean throttle
-pi.set_servo_pulsewidth(19, 1500) # mean throttle
+time.sleep(5)
 
+pi.set_servo_pulsewidth(19, 1000) # min throttle
+time.sleep(1)
+pi.set_servo_pulsewidth(19, 2000) # max throttle
+time.sleep(1)
+pi.set_servo_pulsewidth(19, 1500) # mean throttle
 time.sleep(5)
 
 pi.set_servo_pulsewidth(12, 1450)
